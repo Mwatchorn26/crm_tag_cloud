00c7d6b50ac4e4bccfac3c94b68303434b3a1baf
Michael Watchorn <mwatchorn26@gmail.com>
2014-11-18 09:21:28 -0500

    Delete crm_tag_cloud.py
    no longer needed

--- models/crm_tag_cloud.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp.models import Model
-from openerp.fields import Char
-
-
-class crm_tag_cloud(Model):
-    _name = "crm_tag_cloud.crm_tag_cloud"
-
-    name = Char()
